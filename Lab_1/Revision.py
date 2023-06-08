eb5ccfeb34eeca4037bfbdd967e3540d9341bfd7
Artemiksss <artem.zhovnir@yandex.ru>
2023-06-08 16:13:20 +0300

    DEL FILE

--- Lab_1/Revision.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-nums = input().split(' ')
-int_num = [int(i) for i in nums]
-min_n = min(int_num)
-s_no_min = []
-in_min = int_num.index(min_n)
-int_num.pop(in_min)
-print(min_n, min(int_num))
--- Lab_1/The_last_digit.py
+++
@@ -1,3 +0,0 @@
-num = int(input())
-str_num = str(num)
-print(str_num[-1])
